1ad446ce807904a77450d0e576c3252acef90b80
mubinsyabc <mb_sy@126.com>
2026-09-22 13:01:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -480,6 +480,31 @@ fi
 exit 0
 EOF
 
+cat>files/usr/share/Lenyu-version.sh<<-\EOF
+#!/bin/bash
+# https://github.com/mubinsyabc/immortalwrt-86
+# Actions-OpenWrt-x86 By Lenyu 20210505
+#path=$(dirname $(readlink -f $0))
+# cd ${path}
+#检测准备
+if [ ! -f  "/etc/lenyu_version" ]; then
+echo
+echo -e "\033[31m 该脚本在非Lenyu固件上运行，为避免不必要的麻烦，准备退出… \033[0m"
+echo
+exit 0
+fi
+rm -f /tmp/cloud_version
+# 获取固件云端版本号、内核版本号信息
+current_version=`cat /etc/lenyu_version`
+wget -qO- -t1 -T2 "https://api.github.com/repos/mubinsyabc/immortalwrt-86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+if [ -s  "/tmp/cloud_ts_version" ]; then
+cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
+fi
+exit 0
+EOF
+
+
+
 cat>files/usr/share/Lenyu-pw.sh<<-'EOF_PW'
 #!/bin/sh
 set -u
